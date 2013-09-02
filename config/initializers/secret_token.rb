cadacafb7ebb96b8d7abc44646a66a8405816a9b
laantorchaweb <laantorchaweb@gmail.com>
2013-09-02 13:30:36 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Threebook::Application.config.secret_key_base = 'ba3a5f0e6da21b5cc0ad2d842ef5f2d30848e6bb47d67078f95b2daff6f03b8538f9f269028a71ac50b8920a9ee120af1e624025b81ec9f647474231bc4c0b25'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Threebook::Application.config.session_store :cookie_store, key: '_threebook_session'
